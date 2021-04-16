e0748c73f9f30e4176293c9d7645ab2e8077737d
ViktoriaHultman <81225227+ViktoriaHultman@users.noreply.github.com>
2021-04-16 09:07:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+   var webringSites = [
+        'http://www.empty-frames.com',
+        'http://www.1outof3.se',
+        'http://www.microscopictrance.com',
+        'http://www.amidonenow.com',
+        'http://www.diplomaprinter.biz',
+        'http://www.findivanshobby.com',
+        'http://www.online-ikebana.garden',
+        'http://www.qwertyuiopasdfghjklzxcvbnm.life',
+        'http://www.findtherightdocument.com',
+        'http://www.connectome.nu',
+        'http://www.destiny.exposed',
+        'http://www.wordsdrifting.space',
+        'http://www.cyrillicintersections.com',
+        'http://www.rockyis.training',
+        'http://www.learnaboutmodernart.com',
+        'http://www.persiancarpetstakestime.digital'
+    ];
+
+  function randomWebringSite() {
+        var i = parseInt(Math.random() * webringSites.length);
+        location.href = webringSites[i];
+    }
+
+  function natureWebringInfo() {
+      document.getElementById("natureLogo").src = "https://www.worldwidewebring.club/2021/embed/art-webring-hover.svg";
+    }
+
+  function natureWebringLogo() {
+      document.getElementById("natureLogo").src = "https://www.worldwidewebring.club/2021/embed/art-webring.svg";
+    }
